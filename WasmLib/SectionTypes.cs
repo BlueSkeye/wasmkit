aeb6e8f2f68118117e9693748325aaa846770800
BlueSkeye <bsgithub@gmail.com>
2018-01-11 21:39:18 +0100

    Added section types enumeration

---
+++ WasmLib/SectionTypes.cs
@@ -0,0 +1,29 @@
+﻿namespace WasmLib
+{
+    public enum SectionTypes : byte
+    {
+        Unknown = 0,
+        /// <summary>Function signature declarations</summary>
+        Type = 1,
+        /// <summary>Import declarations</summary>
+        Import = 2,
+        /// <summary>Function declarations</summary>
+        Function = 3,
+        /// <summary>Indirect function table and other tables</summary>
+        Table = 4,
+        /// <summary>Memory attributes</summary>
+        Memory = 5,
+        /// <summary>Global declarations</summary>
+        Global = 6,
+        /// <summary>Exports</summary>
+        Export = 7,
+        /// <summary>Start function declaration</summary>
+        Start = 8,
+        /// <summary>Elements section</summary>
+        Element = 9,
+        /// <summary>Function bodies (code)</summary>
+        Code = 10,
+        /// <summary>Data segments</summary>
+        Data = 11,
+    }
+}
